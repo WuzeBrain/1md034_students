ebc386f607531d336fc1fdc313e937e9836454f1
Samuel Adolfsson <43405750+WuzeBrain@users.noreply.github.com>
2020-02-07 17:40:11 +0100

    Move menu to external file

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,54 +1,6 @@
 const vm = new Vue({
     el: "#burgers",
     data: {
-        menu: null
+        menu: food
     }
-})
-
-class MenuItem {
-    constructor(name, img, size, desc, info) {
-        this.name = name;
-        this.img = img;
-        this.size = size;
-        this.desc = desc;
-        this.info = info;
-    }
-}
-
-vm.menu = [
-    new MenuItem(
-        "Nothing Burger",
-        "img/nothingburger.jpg",
-        120,
-        "Only salad. Nothing more, nothing less.",
-        "May contain traces of nuts."
-    ),
-    new MenuItem(
-        "Veggie Burger",
-        "img/gigaburger.jpg",
-        90,
-        "Contains an assortment of locally produced vegetables.",
-        "100% vegan."
-    ),
-    new MenuItem(
-        "Quad Burger",
-        "img/quadburger.jpg",
-        128,
-        "Also available in a 50% larger size (Hexa Burger).",
-        "101% non-vegan."
-    ),
-    new MenuItem(
-        "sqrt(-1)",
-        "img/void.jpg",
-        0,
-        "Contains whatever you want.",
-        "Safe for everyone."
-    ),
-    new MenuItem(
-        "'Murica",
-        "img/murica.jpg",
-        1776,
-        "F**k Yeah!",
-        "Eat at your own risk."
-    )
-];
+});
