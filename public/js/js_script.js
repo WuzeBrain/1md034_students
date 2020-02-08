12ef3bc3bb252293b67ec5116cd644bda8c64ee2
Samuel Adolfsson <43405750+WuzeBrain@users.noreply.github.com>
2020-02-08 22:00:21 +0100

    Add functionality to send-button

---
+++ public/js/js_script.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// function valueOf(id) {
+//     return document.getElementById(id).value;
+// }
+//
+// document.getElementById("send").onclick = function() {
+//     console.log("Button Clicked!");
+//     let form = [
+//         "fullname",
+//         "email",
+//         "street",
+//         "house",
+//         "payment"
+//     ].map(valueOf);
+//     for (let b of document.getElementsByName("ge")) {
+//         if (b.checked) {
+//             form.push(b.value);
+//         }
+//     }
+//     console.log(form);
+//     return this.value;
+// };
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,6 +1,36 @@
+"use strict"
+
 const vm = new Vue({
-    el: "#burgers",
+    el: "main",
     data: {
-        menu: food
+        menu: food,
+        order: null,
+        fullname: "",
+        email: "",
+        street: "",
+        house: "",
+        payment: "Bitcoin",
+        gender: "undisclosed",
+        burgers: []
+    },
+    methods: {
+        send: function () {
+            console.log("Button Clicked!");
+            this.order = new Map(
+                [
+                    "fullname",
+                    "email",
+                    "street",
+                    "house",
+                    "payment",
+                    "gender",
+                    "burgers"
+                ].map(
+                    x => [x, vm[x]]
+                )
+            );
+            console.log(this.order);
+        },
+        show: x => x.join(": ")
     }
 });
