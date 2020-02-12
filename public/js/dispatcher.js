5b7d8a04ab572f0d4669ab5de4c6b6704a14b02a
Samuel Adolfsson <43405750+WuzeBrain@users.noreply.github.com>
2020-02-12 21:01:34 +0100

    Create Order class, refactor

--- public/js/dispatcher.js
+++ public/js/dispatcher.js
@@ -1,15 +1,19 @@
-/* jslint es6:true, indent: 2 */
+/* jslint es6:true, indent: 4 */
 /* global Vue, io */
 /* exported vm */
 'use strict';
+
 const socket = io();
 
 /* eslint-disable-next-line no-unused-vars */
 const vm = new Vue({
     el: '#orders',
+
     data: {
         orders: {},
+        selectedOrder: null
     },
+
     created: function() {
         socket.on('initialize', function(data) {
             this.orders = data.orders;
@@ -19,7 +23,8 @@ const vm = new Vue({
             this.orders = data.orders;
         }.bind(this));
     },
+
     methods: {
-        formatInfo: (info) => info[0] + " (" + info.slice(1) + ")"
+        formatInfo: (info) => info.fullname + " (" + info.email + ", " + info.payment + ", " + info.gender + ")",
     }
 });
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,63 +1,65 @@
+/* jslint es6:true, indent: 4 */
+/* global Vue, io, food */
+/* exported vm*/
 "use strict";
+
+class Order {
+    constructor() {
+        this.mapPos = null;
+        this.items = [];
+        this.info = {
+            fullname: "",
+            email: "",
+            payment: "Bitcoin",
+            gender: "Undisclosed"
+        };
+    }
+    finished() {
+        if (this.mapPos === null || this.items.length === 0) {
+            return false;
+        }
+        for (let val of Object.values(this.info)) {
+            if (val === "") {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
 const socket = io();
 
+/* eslint-disable-next-line no-unused-vars */
 const vm = new Vue({
     el: "main",
 
     data: {
         menu: food,
-        order: {
-            details: {x: 0, y: 0},
-            items: [],
-            info: [],
-        },
-        fullname: "",
-        email: "",
-        payment: "Bitcoin",
-        gender: "Undisclosed",
-        burgers: [],
+        order: new Order(),
+        pastOrders: [],
+        missing: false
     },
 
     methods: {
 
-        /* Return a Map entry as a string. */
-        show: x => x.join(": "),
-
-        updateOrder: function () {
-            /* Update this.order with the form field contents. */
-            this.order.items = this.burgers;
-            this.order.info = [
-                "fullname",
-                "email",
-                "payment",
-                "gender"
-            ].map(
-                x => [x, vm[x]]
-            );
-        },
-
         addOrder: function() {
             /* Send the current order to the server */
-            this.updateOrder();
-            socket.emit('addOrder', {
-                details: this.order.details,
-                orderItems: this.order.items,
-                customerInfo: this.order.info.map(x => x[1]),
-            });
+            let fin = this.order.finished();
+            if (fin) {
+                socket.emit('addOrder', this.order);
+                this.pastOrders.push(this.order);
+                this.order = new Order();
+            }
+            this.missing = !fin;
         },
 
-        displayOrder: function(event) {
-            /* When you click in the map, a click event object is sent as parameter
-            * to the function designated in v-on:click (i.e. this one).
-            * The click event object contains among other things different
-            * coordinates that we need when calculating where in the map the click
-            * actually happened. */
+        updateMapPos: function(event) {
+            /* ... */
             let offset = {
                 x: event.currentTarget.getBoundingClientRect().left,
                 y: event.currentTarget.getBoundingClientRect().top,
             };
-            this.order.details = {
+            this.order.mapPos = {
                 x: event.clientX - 10 - offset.x,
                 y: event.clientY - 10 - offset.y,
             };
